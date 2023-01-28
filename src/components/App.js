345a5bf2b4fc03b6e3561a7e875d2b5e4b0420fa
sara sallat <sallatsara@gmail.com>
2023-01-28 17:02:41 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,14 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-      </Container>
-      <Container header="Example header" textPosition="center">
+    
+      <Container
+        header='Example header'
+        textPosition='center'
+        direction='horizontal'
+        contentPosition='center'
+        
+      >
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
